d72d4ec34f731d928094a01d869e9f2ed55a4b3e
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-11 00:07:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /
+    	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
+    	<meta name="description" content="">
+    	<meta name="keywords" content="">
     	<title>UAPSA :: MSEUF Chapter</title>
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
         <link href="css/main.css" rel="stylesheet" type="text/css"/>
