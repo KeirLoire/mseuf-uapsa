75f0193d8c4dea9ff69e167ca2d4ba9df950b5ec
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-11 00:00:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,118 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>MSEUF UAPSA</title>
-        <style type="text/css">
-        header {
-            background-color:black;
-            color:white;
-            text-align:center;
-            padding:5px;
-            
-        }
-        nav {
-            line-height:30px;
-            background-color:#eeeeee;
-            height:300px;
-            width:100px;
-            float:left;
-            padding:5px;
-            
-        }
-        section {
-            width:350px;
-            float:left;
-            padding:10px; 
-            
-        }
-        footer {
-            background-color:black;
-            color:white;
-            clear:both;
-            text-align:center;
-            padding:5px;
-            
-        }
-        </style>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /
+    	<title>UAPSA :: MSEUF Chapter</title>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="css/main-responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/themify-icons.css" rel="stylesheet" type="text/css"/>
+        <link href="css/jquery-te.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery-te.js"></script>
+        <script type="text/javascript" src="js/lightbox.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/simplemodal.js"></script>
     </head>
-    <body>
-        <header>
-            <h1>City Gallery</h1>
-        </header>
-        <nav>
-            London<br>
-            Paris<br>
-            Tokyo
-        </nav>
-        <section>
-            <h1>London</h1>
-            <p>London is the capital city of England. It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-            <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-        </section>
-        <footer>
-            Copyright © W3Schools.com
-        </footer>
-    </body>
+	<body>
+    	<div class="main">
+        	<div class="inner">
+            	<div class="header">
+            		<div class="inner">
+                    	<h1 class="logo">
+                        	<a href="index.php">
+                            	<img src="images/logo-uapsa-mseuf.jpg" alt="UAPSA MSEUF Logo" />
+                                <div>
+                                United Architects of the Philippines Student Auxillary
+                                	<span>
+                                		Manuel S. Enverga University Foundation Chapter
+                                    </span>
+                                </div>
+                            </a>
+                    	</h1>
+                	</div>
+                    <div class="clear"></div>
+            	</div>
+           		<div class="navigation">
+            		<div class="inner">
+                		<ul class="nav">
+                        	<li>
+                            	<a href="index.php" class="active">Home</a>
+                            </li>
+                            <li>
+                            	<a href="about/index.php">About</a>
+                                <ul>
+                                	<li>
+                                    	<a href="about/vision-mission-objectives/index.php">Vision, Mission, Objectives</a>
+                                    </li>
+                                    <li>
+                                    	<a href="about/affiliations/index.php">Affiliations</a>
+                                    </li>
+                                    <li>
+                                    	<a href="about/awards-and-recognitions/index.php">Awards and Recognitions</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                            	<a href="events/index.php">Events</a>
+                            </li>
+                            <li>
+                            	<a href="members/index.php">Members</a>
+                            </li>
+                            <li>
+                            <a href="gallery/index.php">Gallery</a>
+                            </li>
+                            <li>
+                            <a href="contact/index.php">Contact Us</a>
+                            </li>
+                        </ul>
+                        <div class="clear"></div>
+            	    </div>
+            	</div>
+          		<div class="banner-holder">
+            		<div class="inner">
+                		<div class="banner" style="height:334.375px;">
+                        	<ul class="slider-nav">
+                            	<li class="n1" onclick="selectSlide(1)" class="normal"></li>
+                                <li class="n2" onclick="selectSlide(2)" class="normal"></li>
+                                <li class="n3" onclick="selectSlide(3)" class="normal"></li>
+                                <li class="n4" onclick="selectSlide(4)" class="normal"></li>
+                            </ul>
+                            <ul id="slider" style="width: 4280px; 0px;">
+                            	<li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner1.jpg" alt="Banner 1" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner2.jpg" alt="Banner 2" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner3.jpg" alt="Banner 3" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner4.jpg" alt="Banner 4" />
+                                </li>
+                            </ul>
+                        </div>
+                	</div>
+            	</div>
+            	<div class="body">
+            		<div class="inner">
+                    	<div class="clear"></div>
+                        <div class="home leftcol">
+                        	
+                        </div>
+                        <div class="home rightcol">
+                        
+                        </div>
+                    	
+                	</div>
+            	</div>
+            	<div class="footer">
+            		<div class="inner">
+                    	
+                	</div>
+            	</div>
+            </div>
+        </div>
+	</body>
 </html>
