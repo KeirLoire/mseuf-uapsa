f092207fad3a5e1075088909ce4987ac486e7717
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-11 22:36:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-    	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
-    	<meta name="description" content="United Architects of the Philippines Student Auxiliary (UAPSA) is the student organization of the United Architects of the Philippines (UAP) in Manuel S. Enverga University Foundation (MSEUF).">
-    	<meta name="keywords" content="manuel s. enverga university foundation united architects of the philippines student auxiliary uapsa mseuf">
-    	<title>UAPSA :: MSEUF Chapter</title>
-        <link rel="shortcut icon" type="http://uapsa-mseuf.azurewebsites.net/images/x-icon" href="favicon.ico">
-        <link href="http://uapsa-mseuf.azurewebsites.net/css/main.css" rel="stylesheet" type="text/css"/>
-        <link href="http://uapsa-mseuf.azurewebsites.net/css/main-responsive.css" rel="stylesheet" type="text/css"/>
-        <link href="http://uapsa-mseuf.azurewebsites.net/css/themify-icons.css" rel="stylesheet" type="text/css"/>
-        <link href="http://uapsa-mseuf.azurewebsites.net/css/jquery-te.css" rel="stylesheet" type="text/css"/>
-        <script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/jquery.js"></script>
-        <script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/jquery-te.js"></script>
-        <script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/lightbox.js"></script>
-        <script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/main.js"></script>
-        <script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/simplemodal.js"></script>
-    </head>
+    		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
+    		<meta name="description" content="United Architects of the Philippines Student Auxiliary (UAPSA) is the student organization of the United Architects of the Philippines (UAP) in Manuel S. Enverga University Foundation (MSEUF).">
+    		<meta name="keywords" content="manuel s. enverga university foundation united architects of the philippines student auxiliary uapsa mseuf">
+    		<title>UAPSA :: MSEUF Chapter</title>
+        	<link rel="shortcut icon" type="http://uapsa-mseuf.azurewebsites.net/images/x-icon" href="favicon.ico">
+        	<link href="http://uapsa-mseuf.azurewebsites.net/css/main.css" rel="stylesheet" type="text/css"/>
+        	<link href="http://uapsa-mseuf.azurewebsites.net/css/main-responsive.css" rel="stylesheet" type="text/css"/>
+        	<link href="http://uapsa-mseuf.azurewebsites.net/css/themify-icons.css" rel="stylesheet" type="text/css"/>
+        	<link href="http://uapsa-mseuf.azurewebsites.net/css/jquery-te.css" rel="stylesheet" type="text/css"/>
+        	<script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/jquery.js"></script>
+        	<script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/jquery-te.js"></script>
+        	<script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/lightbox.js"></script>
+        	<script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/main.js"></script>
+        	<script type="text/javascript" src="http://uapsa-mseuf.azurewebsites.net/js/simplemodal.js"></script>
+    	</head>
 	<body>
     	<div id="main">
         	<div class="inner">
@@ -82,45 +82,107 @@
                         <div class="clear"></div>
             	    </div>
             	</div>
-          		<div class="banner-holder">
+          		<div id="banner-holder">
             		<div class="inner">
-                		<div class="banner" style="height:334.375px;">
-                        	<ul class="slider-nav">
-                            	<li class="n1" onclick="selectSlide(1)" class="normal selected"></li>
-                                <li class="n2" onclick="selectSlide(2)" class="normal"></li>
+                		<div id="banner" style="height:334.375px;">
+                        	<ul id="slider-nav">
+                            	<li id="n1" onclick="selectSlide(1)" class="normal selected"></li>
+                                <li id="n2" onclick="selectSlide(2)" class="normal"></li>
                             </ul>
-                            <ul id="slider" style="width: 4280px; 0px;">
+                            <ul id="slider" style="width: 2140px; 0px;">
                             	<li class="slide" style="height: 334.375px; width: 1070px;">
                                 	<img src="http://uapsa-mseuf.azurewebsites.net/images/banners/20150921-conex.jpg" alt="Convetion Exhibit" />
                                 </li>
                                 <li class="slide" style="height: 334.375px; width: 1070px;">
                                 	<img src="http://uapsa-mseuf.azurewebsites.net/images/banners/20151126-naw-national-artist.jpg" alt="Paghugpong Bonding Together Towards Higher Ground" />
                                 </li>
-                                </li>
                             </ul>
                         </div>
                 	</div>
             	</div>
-            	<div class="body">
-            		<div class="inner">
-                    	<div class="clear"></div>
-                        <div class="home leftcol">
-                        	
-                        </div>
-                        <div class="home rightcol">
-                        
-                        </div>
-                    	
-                	</div>
-            	</div>
-            	<div class="footer">
-            		<div class="inner">
-            			<h1>
-            				<span>United Architects of the Philippines Student Auxillary &copy 2016<br>Manuel S. Enverga University Foundation Chapter</span>
-            			</h1>
-                	</div>
-            	</div>
-            </div>
+            		<div id="body">
+            			<div class="inner">
+                    			<div class="clear"></div>
+                        		<div class="home leftcol">
+                        			<div class="section-wrapper">
+                            				<ul>
+                                				<li onclick="selectHomeSection('announcements')" id="nav-section-announcements" class="selected">Announcements</li>
+                                    				<li onclick="selectHomeSection('news')" id="nav-section-news" class="normal">News</li>
+                                				<li onclick="selectHomeSection('events')" id="nav-section-events" class="normal">Events</li>
+                                			</ul>
+                                			<div class="clear"></div>
+                                			<div id="home-announcements" class="section" style="display: block;">
+                                				<div class="content">
+                                    					<div class="announcement">
+                                        					<div class="header">Creation of UAPSA Website</div>
+                                            					<div class="data">January 10, 2016</div>
+                                            					<div class="body">
+                                            						<p>UAPSA MSEUF Chapter</p>
+                                            						<p>"We would like to thank the people helped us in creating and designing this website.</p>
+                                            						<div class="clear"></div>
+                                            						<div class="clear"></div>
+                                            						<br>
+                                        					</div>
+                                        					<div class="clear"></div>
+                                  					</div>
+                                  				</div>
+                                  				<div class="clear"></div>
+                                  			</div>
+                                			<div id="home-news" class="section" style="display: none;">
+                                  				<div class="content">
+                                    					
+                                    				</div>
+                                				<div class="clear"></div>
+                                			</div>
+                                  			<div id="home-events" class="section" style="display: none;">
+                                  				<div class="content">
+                                    					
+                                    				</div>
+                                    				<div class="clear"></div>
+                                  			</div>
+                            			</div>
+                        		</div>
+                			<div class="home rightcol">
+                        			<div class="widget videos">
+                            				<div class="header">Videos</div>
+                                			<div class="content">
+                                				
+                                			</div>
+                            			</div>
+                            			<div class="widget photos">
+                            				<div class="header">Photos</div>
+                            			</div>
+                			</div>
+                    			<div class="clear"></div>
+                        		<br>
+                        		<br>
+                        		<br>
+                        		<div id="partners">
+                        			<div class="header">
+                            				<div class="context">
+                                				<h2>Partners</h2>
+                        				</div>
+                                			<div class="clear"</div>
+                            			</div>
+                            			<div class="content">
+                            				<div class="partner">
+                                				<a href="http://united-architects.org/" target="_blank">
+                                    					<img src="http://uapsa-mseuf.azurewebsites.net/images/logo-uap.jpg" alt="0" />
+                                    				</a>
+                                			</div>
+                            			</div>
+                        		</div>
+                        		<div class="clear"></div>
+                		</div>
+            		</div>
+            		<div id="footer">
+            			<div class="inner">
+            				<h1>
+            					<span>United Architects of the Philippines Student Auxillary &copy 2016<br>Manuel S. Enverga University Foundation Chapter</span>
+            				</h1>
+                		</div>
+            		</div>
+        	</div>
         </div>
 	</body>
 </html>
