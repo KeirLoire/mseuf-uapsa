b9158204f1c7ac7bb10c8e47238333b70ac7dd38
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-11 12:43:19 +0800

    Update index.php

--- about/index.php
+++ about/index.php
@@ -1,1 +1,120 @@
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+    	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
+    	<meta name="description" content="">
+    	<meta name="keywords" content="">
+    	<title>UAPSA :: MSEUF Chapter</title>
+        <link rel="shortcut icon" type="images/x-icon" href="favicon.ico">
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="css/main-responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/themify-icons.css" rel="stylesheet" type="text/css"/>
+        <link href="css/jquery-te.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery-te.js"></script>
+        <script type="text/javascript" src="js/lightbox.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/simplemodal.js"></script>
+    </head>
+	<body>
+    	<div class="main">
+        	<div class="inner">
+            	<div class="header">
+            		<div class="inner">
+                    		<h1 class="logo">
+                        		<a href="http://mseufuapsa.azurewebsites.net/">
+                            			<img src="images/logo-uapsa-mseuf.jpg" alt="UAPSA MSEUF Logo" />
+                        			<div>
+                        				United Architects of the Philippines Student Auxillary
+                        				<span>Manuel S. Enverga University Foundation Chapter</span>
+                        			</div>
+                        			</a>
+                    		</h1>
+                	</div>
+                    <div class="clear"></div>
+            	</div>
+           		<div class="navigation">
+            		<div class="inner">
+                		<ul class="nav">
+                        	<li>
+                            	<a href="index.php">Home</a>
+                            </li>
+                            <li>
+                            	<a href="about/index.php" class="active">About</a>
+                                <ul>
+                                	<li>
+                                    	<a href="about/vision-mission-objectives/index.php">Vision, Mission, Objectives</a>
+                                    </li>
+                                    <li>
+                                    	<a href="about/affiliations/index.php">Affiliations</a>
+                                    </li>
+                                    <li>
+                                    	<a href="about/awards-and-recognitions/index.php">Awards and Recognitions</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                            	<a href="events/index.php">Events</a>
+                            </li>
+                            <li>
+                            	<a href="members/index.php">Members</a>
+                            </li>
+                            <li>
+                            <a href="gallery/index.php">Gallery</a>
+                            </li>
+                            <li>
+                            <a href="contact/index.php">Contact Us</a>
+                            </li>
+                        </ul>
+                        <div class="clear"></div>
+            	    </div>
+            	</div>
+          		<div class="banner-holder">
+            		<div class="inner">
+                		<div class="banner" style="height:334.375px;">
+                        	<ul class="slider-nav">
+                            	<li class="n1" onclick="selectSlide(1)" class="normal"></li>
+                                <li class="n2" onclick="selectSlide(2)" class="normal"></li>
+                                <li class="n3" onclick="selectSlide(3)" class="normal"></li>
+                                <li class="n4" onclick="selectSlide(4)" class="normal"></li>
+                            </ul>
+                            <ul id="slider" style="width: 4280px; 0px;">
+                            	<li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner1.jpg" alt="Banner 1" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner2.jpg" alt="Banner 2" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner3.jpg" alt="Banner 3" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="images/banners/banner4.jpg" alt="Banner 4" />
+                                </li>
+                            </ul>
+                        </div>
+                	</div>
+            	</div>
+            	<div class="body">
+            		<div class="inner">
+                    	<div class="clear"></div>
+                        <div class="home leftcol">
+                        	
+                        </div>
+                        <div class="home rightcol">
+                        
+                        </div>
+                    	
+                	</div>
+            	</div>
+            	<div class="footer">
+            		<div class="inner">
+            			<h1>
+            				<span>Manuel S. Enverga University Foundation Chapter</span>	
+            			</h1>
+                	</div>
+            	</div>
+            </div>
+        </div>
+	</body>
+</html>
