aa32f53661b900d84ec0735ff700f2fad6d607c6
Guthix1991 <flippyveteranstorm@icloud.com>
2016-01-24 16:44:55 +0800

    Revert "Update content"
    This reverts commit 774f824136296e7bb048556b2043dfb671840433.

--- about/index.php
+++ about/index.php
@@ -1,132 +1,120 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-    		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
-    		<meta name="description" content="United Architects of the Philippines Student Auxiliary (UAPSA) is the student organization of the United Architects of the Philippines (UAP) in Manuel S. Enverga University Foundation (MSEUF).">
-    		<meta name="keywords" content="manuel s. enverga university foundation united architects of the philippines student auxiliary uapsa mseuf">
-    		<title>UAPSA :: MSEUF Chapter</title>
-        	<link rel="shortcut icon" type="images/x-icon" href="../support/favicon.ico">
-        	<link href="../css/main.css" rel="stylesheet" type="text/css"/>
-        	<link href="../css/main-responsive.css" rel="stylesheet" type="text/css"/>
-        	<link href="../css/themify-icons.css" rel="stylesheet" type="text/css"/>
-        	<link href="../css/jquery-te.css" rel="stylesheet" type="text/css"/>
-        	<script type="text/javascript" src="../js/jquery.js"></script>
-        	<script type="text/javascript" src="../js/jquery-te.js"></script>
-        	<script type="text/javascript" src="../js/lightbox.js"></script>
-        	<script type="text/javascript" src="../js/main.js"></script>
-        	<script type="text/javascript" src="../js/simplemodal.js"></script>
-    	</head>
+    	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8">
+    	<meta name="description" content="">
+    	<meta name="keywords" content="">
+    	<title>UAPSA :: MSEUF Chapter</title>
+        <link rel="shortcut icon" type="images/x-icon" href="favicon.ico">
+        <link href="http://mseufuapsa.azurewebsites.net/css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="http://mseufuapsa.azurewebsites.net/css/main-responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="http://mseufuapsa.azurewebsites.net/css/themify-icons.css" rel="stylesheet" type="text/css"/>
+        <link href="http://mseufuapsa.azurewebsites.net/css/jquery-te.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="http://mseufuapsa.azurewebsites.net/js/jquery.js"></script>
+        <script type="text/javascript" src="http://mseufuapsa.azurewebsites.net/js/jquery-te.js"></script>
+        <script type="text/javascript" src="http://mseufuapsa.azurewebsites.net/js/lightbox.js"></script>
+        <script type="text/javascript" src="http://mseufuapsa.azurewebsites.net/js/main.js"></script>
+        <script type="text/javascript" src="http://mseufuapsa.azurewebsites.net/js/simplemodal.js"></script>
+    </head>
 	<body>
-    	<div id="main">
+    	<div class="main">
         	<div class="inner">
-            	<div id="header">
+            	<div class="header">
             		<div class="inner">
-                    		<h1 id="logo">
-                        		<a href="index.php">
-                            			<img src="../images/logo-uapsa-mseuf.jpg" alt="UAPSA-MSEUF Logo" />
+                    		<h1 class="logo">
+                        		<a href="http://mseufuapsa.azurewebsites.net/">
+                            			<img src="http://mseufuapsa.azurewebsites.net/images/logo-uapsa-mseuf.jpg" alt="UAPSA MSEUF Logo" />
                         			<div>
-                        				United Architects of the Philippines Student Auxiliary
+                        				United Architects of the Philippines Student Auxillary
                         				<span>Manuel S. Enverga University Foundation Chapter</span>
                         			</div>
-                        		</a>
+                        			</a>
                     		</h1>
-                    		<div class="social-media">
-                            	<ul>
-                                	<li><a href="" target="_blank"><img src="../images/icon-social-media-facebook.png" alt="Facebook" /></a></li>
-                                </ul>
-                            </div>
                 	</div>
                     <div class="clear"></div>
             	</div>
-           		<div id="navigation">
+           		<div class="navigation">
             		<div class="inner">
-                		<ul id="nav">
+                		<ul class="nav">
                         	<li>
-                            	<a href="../index.php">Home</a>
+                            	<a href="http://mseufuapsa.azurewebsites.net/">Home</a>
                             </li>
                             <li>
-                            	<a href="../about/index.php" class="active">About</a>
+                            	<a href="http://mseufuapsa.azurewebsites.net/about/" class="active">About</a>
                                 <ul>
                                 	<li>
-                                    	<a href="../about/history/index.php">History</a>
-                                    </li>
-                                	<li>
-                                    	<a href="../about/vision-mission-objectives/index.php">Vision, Mission, Objectives</a>
+                                    	<a href="http://mseufuapsa.azurewebsites.net/about/vision-mission-objectives/">Vision, Mission, Objectives</a>
                                     </li>
                                     <li>
-                                    	<a href="../about/affiliations/index.php">Affiliations</a>
+                                    	<a href="http://mseufuapsa.azurewebsites.net/about/affiliations/">Affiliations</a>
                                     </li>
                                     <li>
-                                    	<a href="../about/awards-and-recognitions/index.php">Awards and Recognitions</a>
+                                    	<a href="http://mseufuapsa.azurewebsites.net/about/awards-and-recognitions/">Awards and Recognitions</a>
                                     </li>
                                 </ul>
                             </li>
                             <li>
-                            	<a href="../events/index.php">Events</a>
+                            	<a href="http://mseufuapsa.azurewebsites.net/events/">Events</a>
                             </li>
                             <li>
-                            	<a href="../officers/index.php">Officers FY 2015-2016</a>
+                            	<a href="http://mseufuapsa.azurewebsites.net/members/">Members</a>
                             </li>
                             <li>
-                            	<a href="../gallery/index.php">Gallery</a>
-                                <ul>
-                                	<li>
-                                    	<a href="../gallery/photos/index.php">Photos</a>
-                                    </li>
-                                    <li>
-                                    	<a href="../gallery/videos/index.php">Videos</a>
-                                    </li>
-                                    <li>
-                                    	<a href="../gallery/activities/index.php">Activities</a>
-                                    </li>
-                                </ul>
+                            <a href="http://mseufuapsa.azurewebsites.net/gallery/">Gallery</a>
                             </li>
                             <li>
-                            	<a href="../contact-us/index.php">Contact Us</a>
+                            <a href="http://mseufuapsa.azurewebsites.net/contact/">Contact Us</a>
                             </li>
                         </ul>
-                        <div class="search">
-                        	<form method="post" action="#">
-                            	<input type="text" placeholder="Enter keywords" />
-                                <input type="submit" value="Search" />
-                            </form>
-                        </div>
                         <div class="clear"></div>
             	    </div>
             	</div>
-            		<div id="body">
-            			<div class="inner">
-                    			<div class="clear"></div>
-                        		
-                        		</div>
-                                <div class="home rightcol">
-                        			<div class="widget videos">
-                            				<div class="header">Videos</div>
-                                			<div class="content">
-                                				
-                                			</div>
-                            			</div>
-                            			<div class="widget photos">
-                            				<div class="header">Photos</div>
-                            			</div>
-                				</div>
-                        		<div class="clear"></div>
+          		<div class="banner-holder">
+            		<div class="inner">
+                		<div class="banner" style="height:334.375px;">
+                        	<ul class="slider-nav">
+                            	<li class="n1" onclick="selectSlide(1)" class="normal"></li>
+                                <li class="n2" onclick="selectSlide(2)" class="normal"></li>
+                                <li class="n3" onclick="selectSlide(3)" class="normal"></li>
+                                <li class="n4" onclick="selectSlide(4)" class="normal"></li>
+                            </ul>
+                            <ul id="slider" style="width: 4280px; 0px;">
+                            	<li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="http://mseufuapsa.azurewebsites.net/images/banners/banner1.jpg" alt="Banner 1" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="http://mseufuapsa.azurewebsites.net/images/banners/banner2.jpg" alt="Banner 2" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="http://mseufuapsa.azurewebsites.net/images/banners/banner3.jpg" alt="Banner 3" />
+                                </li>
+                                <li class="slide" style="height: 334.375px; width: 1070px;">
+                                	<img src="http://mseufuapsa.azurewebsites.net/images/banners/banner4.jpg" alt="Banner 4" />
+                                </li>
+                            </ul>
                         </div>
-            		</div>
-            		<div id="footer">
-            			<div class="inner">
-            				<div class="clear"></div>
-                            <div id="content">
-            					<h1>
-            						<div>
-            							<span>United Architects of the Philippines Student Auxiliary &copy 2016<br>Manuel S. Enverga University Foundation, University Site, Lucena City, Philippines</span>
-            						</div>
-            					</h1>
-                            </div>
-            				<div class="clear"></div>
-                		</div>
-            		</div>
-        	</div>
+                	</div>
+            	</div>
+            	<div class="body">
+            		<div class="inner">
+                    	<div class="clear"></div>
+                        <div class="home leftcol">
+                        	
+                        </div>
+                        <div class="home rightcol">
+                        
+                        </div>
+                    	
+                	</div>
+            	</div>
+            	<div class="footer">
+            		<div class="inner">
+            			<h1>
+            				<span>United Architects of the Philippines Student Auxillary &copy 2016<br>Manuel S. Enverga University Foundation Chapter</span>	
+            			</h1>
+                	</div>
+            	</div>
+            </div>
         </div>
 	</body>
 </html>
