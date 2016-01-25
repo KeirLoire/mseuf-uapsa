30975795620beb2379aaa6b6898d107c9d2a2daf
Chester Ayala <flippyveteranstorm@icloud.com>
2016-01-25 16:19:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -41,34 +41,34 @@
    		<div id="navigation">
 	    		<div class="inner">
 	        		<ul id="nav">
-	                	<li><a href="index.php" class="active">Home</a></li>
-	                    	<li><a href="about/index.php">About</a><ul>
-	                        	<li><a href="../about/history/index.php">History</a></li>
-	                        	<li><a href="about/vision-mission-objectives/index.php">Vision, Mission, Objectives</a></li>
-	                        	<li><a href="about/affiliations/index.php">Affiliations</a></li>
-	                        	<li><a href="about/awards-and-recognitions/index.php">Awards and Recognitions</a></li>
-	                        </ul></li>
-	                        <li><a href="events/index.php">Events</a></li>
-	                        <li><a href="officers/index.php">Officers FY 2015-2016</a></li>
-	                        <li><a href="gallery/index.php">Gallery</a><ul>
-	                        	<li><a href="../gallery/photos/index.php">Photos</a></li>
-	                        	<li><a href="../gallery/videos/index.php">Videos</a></li>
-	                        	<li><a href="../gallery/activities/index.php">Activities</a></li>
-	                        </ul></li>
-	                        <li><a href="http://uapsa-mseuf.azurewebsites.net/members/">Officers FY 2015-2016</a></li>
-	                	<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/">Gallery</a><ul>
-	                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/photos/">Photos</a></li>
-	                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/videos/">Videos</a></li>
-	                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/activities/">Activities</a></li>
-	                    	</ul></li>
-	                    	<li><a href="contact-us/index.php">Contact Us</a></li>
-	                	</ul>
-	                        <div class="search">
-	                        	<form method="post" action="#">
-		                            	<input type="text" placeholder="Enter keywords" />
-		                                <input type="submit" value="Search" />
-	                    		</form>
-	                        </div>
+		                	<li><a href="index.php" class="active">Home</a></li>
+		                    	<li><a href="about/index.php">About</a><ul>
+		                        	<li><a href="../about/history/index.php">History</a></li>
+		                        	<li><a href="about/vision-mission-objectives/index.php">Vision, Mission, Objectives</a></li>
+		                        	<li><a href="about/affiliations/index.php">Affiliations</a></li>
+		                        	<li><a href="about/awards-and-recognitions/index.php">Awards and Recognitions</a></li>
+		                        </ul></li>
+		                        <li><a href="events/index.php">Events</a></li>
+		                        <li><a href="officers/index.php">Officers FY 2015-2016</a></li>
+		                        <li><a href="gallery/index.php">Gallery</a><ul>
+		                        	<li><a href="../gallery/photos/index.php">Photos</a></li>
+		                        	<li><a href="../gallery/videos/index.php">Videos</a></li>
+		                        	<li><a href="../gallery/activities/index.php">Activities</a></li>
+		                        </ul></li>
+		                        <li><a href="http://uapsa-mseuf.azurewebsites.net/members/">Officers FY 2015-2016</a></li>
+		                	<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/">Gallery</a><ul>
+		                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/photos/">Photos</a></li>
+		                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/videos/">Videos</a></li>
+		                    		<li><a href="http://uapsa-mseuf.azurewebsites.net/gallery/activities/">Activities</a></li>
+		                    	</ul></li>
+		                    	<li><a href="contact-us/index.php">Contact Us</a></li>
+		                        <div class="search">
+		                        	<form method="post" action="#">
+			                            	<input type="text" placeholder="Enter keywords" />
+			                                <input type="submit" value="Search" />
+		                    		</form>
+		                        </div>
+	                        </ul>
 	                <div class="clear"></div>
 	    	    	</div>
             	</div>
